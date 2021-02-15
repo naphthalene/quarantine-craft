dbd4bfbeb1b2f239192eb4835fb669b533f98f37
Pasha <sigmat@protonmail.com>
2021-02-15 08:49:56 -0500

    Initial Website changes

---
+++ web/README.md
@@ -0,0 +1,15 @@
+# Quarantine Craft Homepage
+
+This is the homepage of the server. Motivation for this website is also
+to display player and server stats and provide a place for updates.
+
+## Local Development
+
+    npm i
+    npm run dev
+
+## Stats
+
+A scraper will periodically run to collect information from server
+config files and databases, and will copy the data to a location served
+by nginx from the same host.
---
+++ web/components/layout.js
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+// import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = '[ Quarantine Craft ]';
+export const siteTitle = 'Quarantine Craft';
+// const headerPic = "/images/qcraft.png";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            {/* <Image */}
+            {/*   priority */}
+            {/*   src={headerPic} */}
+            {/*   className={utilStyles.borderCircle} */}
+            {/*   height={144} */}
+            {/*   width={144} */}
+            {/*   alt={name} */}
+            {/* /> */}
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <h2 className={utilStyles.headingLg}>
+              More coming soon...
+            </h2>
+          </>
+        ) : (
+          <>
+            {/* <Link href="/"> */}
+            {/*   <a> */}
+            {/*     <Image */}
+            {/*       priority */}
+            {/*       src={headerPic} */}
+            {/*       className={utilStyles.borderCircle} */}
+            {/*       height={108} */}
+            {/*       width={108} */}
+            {/*       alt={name} */}
+            {/*     /> */}
+            {/*   </a> */}
+            {/* </Link> */}
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
