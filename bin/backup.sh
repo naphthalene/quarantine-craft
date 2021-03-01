3475b1023c9401d8bca2bb031ce8ab909ddabd68
Pasha <sigmat@protonmail.com>
2021-03-01 15:54:21 -0500

    Update scripts to work with new system

--- bin/backup.sh
+++ bin/backup.sh
@@ -10,57 +10,26 @@ IFS=$'\n\t'
 # -------------------------------------------------------
 
 [ -f /etc/default/minecraft ] && . /etc/default/minecraft
-: "${SERVER_ROOT?}"
-: "${BACKUPS_DIR?}"
+: "${DATA_DIR?}"
+: "${BACKUP_DIR?}"
 
 NOW_TS="$(date +%s)"
-BACKUP_TARBALL="${BACKUPS_DIR}/${NOW_TS}.tar"
-PLUGIN_DATA_PATHS=(
-  "AutomatedCrafting/droppers.json"
-  "ChestSort/playerdata/"
-  "dynmap/ids-by-ip.txt"
-  "dynmap/markers.yml"
-  "Essentials/warps/"
-  "Essentials/userdata/"
-  "GriefPreventionData/Logs"
-  "GriefPreventionData/ClaimData"
-  "GriefPreventionData/PlayerData"
-  "LuckPerms/luckperms-h2.mv.db"
-  "LuckPerms/luckperms-h2.trace.db"
-  "mcMMO/flatfile/"
-  "TreeAssist/world/"
-  "TreeAssist/world_nether/"
-  "TreeAssist/world_the_end/"
-)
+BACKUP_TARBALL="${BACKUP_DIR}/${NOW_TS}.tar"
 
 function _tar() {
-  tar -C ${SERVER_ROOT}/ $@
+  tar -C ${DATA_DIR}/ $@
 }
 
 function _create_backup_tarball() {
-  echo ">> Created ${BACKUP_TARBALL}"
+  echo ">> Created [${BACKUP_TARBALL}]"
   _tar -cf "${BACKUP_TARBALL}" -T /dev/null
 }
 
-function _backup_plugins() {
-  echo ">> Backing up plugin data..."
-  for dataPath in "${PLUGIN_DATA_PATHS[@]}"
-  do
-    if [[ -e "${SERVER_ROOT}/plugins/${dataPath}" ]]; then
-      echo ">> + [${dataPath}]"
-      _tar --append -f "${BACKUP_TARBALL}" "plugins/${dataPath}"
-    else
-      echo ">> ! Not found: [${dataPath}]"
-    fi
-  done
-}
-
-function _backup_worlds() {
-  echo ">> Backing up worlds..."
-  _tar --append -f "${BACKUP_TARBALL}" world/ world_nether/ world_the_end/
+function _backup_data_volume() {
+  echo ">> Backing up data volume [${DATA_DIR}]"
+  _tar --append -f "${BACKUP_TARBALL}" *
 }
 
 _create_backup_tarball
-_backup_plugins
-_backup_worlds
+_backup_data_volume
 echo ">> Done"
---
+++ bin/start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative
+  # to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+# -------------------------------------------------------
+# This script runs the server
+# -------------------------------------------------------
+[ -f /etc/default/minecraft ] && . /etc/default/minecraft
+
+java \
+    -Xms10G \
+    -Xmx10G \
+    -XX:+UseG1GC \
+    -XX:+ParallelRefProcEnabled \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+DisableExplicitGC \
+    -XX:+AlwaysPreTouch \
+    -XX:G1NewSizePercent=30 \
+    -XX:G1MaxNewSizePercent=40 \
+    -XX:G1HeapRegionSize=8M \
+    -XX:G1ReservePercent=20 \
+    -XX:G1HeapWastePercent=5 \
+    -XX:G1MixedGCCountTarget=4 \
+    -XX:InitiatingHeapOccupancyPercent=15 \
+    -XX:G1MixedGCLiveThresholdPercent=90 \
+    -XX:G1RSetUpdatingPauseTimePercent=5 \
+    -XX:SurvivorRatio=32 \
+    -XX:+PerfDisableSharedMem \
+    -XX:MaxTenuringThreshold=1 \
+    -jar "${DIR}/paper.jar" nogui
